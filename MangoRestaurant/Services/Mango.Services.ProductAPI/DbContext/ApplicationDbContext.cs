3591db22b7155514e586f08b320f6c5df0bfdf5e
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2022-09-05 08:21:00 +0300

    Configured db and migrate  products table

---
+++ MangoRestaurant/Services/Mango.Services.ProductAPI/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿namespace Mango.Services.ProductAPI.DbContext;
+
+using Mango.Services.ProductAPI.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+
+    }
+
+    public DbSet<Product> Products { get; set; }
+}

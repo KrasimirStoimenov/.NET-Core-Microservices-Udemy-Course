c04c8106e3c3c5110aaaaa2788980b41021747a6
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2022-09-09 13:50:26 +0300

    Create products controller and product index view page

---
+++ MangoRestaurant/FrontEnd/Mango.Web/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿namespace Mango.Web.Controllers;
+
+using Mango.Web.Models;
+using Mango.Web.Services.Interfaces;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Newtonsoft.Json;
+
+public class ProductController : Controller
+{
+    private readonly IProductService productService;
+
+    public ProductController(IProductService productService)
+    {
+        this.productService = productService;
+    }
+
+    public async Task<IActionResult> ProductIndex()
+    {
+        List<ProductModel> products = new List<ProductModel>();
+
+        var response = await this.productService.GetAllProductsAsync<ResponseModel>();
+        if (response != null && response.IsSuccess)
+        {
+            products = JsonConvert.DeserializeObject<List<ProductModel>>(Convert.ToString(response.Result));
+        }
+
+        return View(products);
+    }
+}

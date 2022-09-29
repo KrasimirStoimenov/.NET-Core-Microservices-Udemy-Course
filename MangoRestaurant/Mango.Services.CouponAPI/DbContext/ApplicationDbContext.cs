3b2cb9ce32b0be012a315b9a83c4b6552c32b502
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2022-09-29 10:09:16 +0300

    Seed Coupon Database

--- MangoRestaurant/Mango.Services.CouponAPI/DbContext/ApplicationDbContext.cs
+++ MangoRestaurant/Mango.Services.CouponAPI/DbContext/ApplicationDbContext.cs
@@ -13,4 +13,23 @@ public class ApplicationDbContext : DbContext
     }
 
     public DbSet<Coupon> Coupons { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Coupon>().HasData(new Coupon
+        {
+            CouponId = 1,
+            CouponCode = "10OFF",
+            DiscountAmount = 10
+        });
+
+        modelBuilder.Entity<Coupon>().HasData(new Coupon
+        {
+            CouponId = 2,
+            CouponCode = "2OFF",
+            DiscountAmount = 20
+        });
+    }
 }

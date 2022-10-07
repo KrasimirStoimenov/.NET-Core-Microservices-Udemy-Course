e287b60376b60329c92122d36219573f761db19c
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2022-10-07 09:44:45 +0300

    Added DbContext and migrate database

---
+++ MangoRestaurant/Services/Mango.Services.OrderAPI/AutoMappingProfile/MappingProfile.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.Services.OrderAPI.AutoMappingProfile;
+
+using AutoMapper;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+    }
+}
---
+++ MangoRestaurant/Services/Mango.Services.OrderAPI/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿namespace Mango.Services.OrderAPI.DbContext;
+
+using Mango.Services.OrderAPI.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+
+    }
+
+    public DbSet<OrderHeader> OrderHeaders { get; set; }
+    public DbSet<OrderDetails> OrderDetails { get; set; }
+}

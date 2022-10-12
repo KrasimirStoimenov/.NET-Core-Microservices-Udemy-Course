833c9b1be82f8094e8967cb489cd1268f93f5359
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2022-10-12 10:21:43 +0300

    Configure Ocelot

--- MangoRestaurant/Gateway/Mango.OcelotAPIGateway/Program.cs
+++ MangoRestaurant/Gateway/Mango.OcelotAPIGateway/Program.cs
@@ -1,6 +1,24 @@
+using Microsoft.IdentityModel.Tokens;
+
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = builder.Configuration.GetValue<string>("ServiceUrls:IdentityAPI");
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddOcelot();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+await app.UseOcelot();
 
 app.Run();
